9ddb855f16b589752cfa1e3f7d62c56a4952f478
Jay-R-H <jayhiremathr@gmail.com>
2021-10-09 19:37:26 +0530

    Added:30 and 56 to 70

--- src/com/programming/LinkedListRunner.java
+++ src/com/programming/LinkedListRunner.java
@@ -1,21 +1,24 @@
 package com.programming;
 
 public class LinkedListRunner {
+
+
     public static void main(String[] args) {
 
-        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        LinkedListOperation linked_list = new LinkedListOperation();
+
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
-        MyNode<Integer> myThirdNode = new MyNode<>(70);
-        // Linking nodes.
-        myFirstNode.setNext(mySecondNode);
-        mySecondNode.setNext(myThirdNode);
-        // Checking if nodes are linked successfully.
-        if (myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode))
-            System.out.println("Linked successfully");
-        else
-            System.out.println("Linking unsuccessful");
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        //Adding nodes.
+        linked_list.addNode(myFirstNode);
+        linked_list.addNode(mySecondNode);
+        linked_list.addNode(myThirdNode);
+        //Displaying the nodes.
+        linked_list.displayNode();
 
     }
 
 }
+
 
