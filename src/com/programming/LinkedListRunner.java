c31822132fa92d2f4fcd98447972219a42f27a6d
Jay-R-H <jayhiremathr@gmail.com>
2021-10-09 19:41:35 +0530

    Added:Appending 30 and 70 to 56

--- src/com/programming/LinkedListRunner.java
+++ src/com/programming/LinkedListRunner.java
@@ -4,16 +4,15 @@ public class LinkedListRunner {
 
 
     public static void main(String[] args) {
-
         LinkedListOperation linked_list = new LinkedListOperation();
 
-        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
-        MyNode<Integer> myThirdNode = new MyNode<>(56);
-        //Adding nodes.
-        linked_list.addNode(myFirstNode);
-        linked_list.addNode(mySecondNode);
-        linked_list.addNode(myThirdNode);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        //Appending nodes.
+        linked_list.appendNode(myFirstNode);
+        linked_list.appendNode(mySecondNode);
+        linked_list.appendNode(myThirdNode);
         //Displaying the nodes.
         linked_list.displayNode();
 
